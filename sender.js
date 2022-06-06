cec23884bcc0e063f94c4fa77b812e65c6593240
ShohruxOralov <100790080+ShohruxOralov@users.noreply.github.com>
2022-06-06 14:40:19 +0500

    Add files via upload

---
+++ sender.js
@@ -0,0 +1,84 @@
+//bot token
+var telegram_bot_id = "5360658342:AAEhPAMKa42ox03cDGVacNspjtLPk2q9th8";
+//chat id
+var chat_id = -1001633946647;
+var u_name, email, message;
+var ready = function () {
+    u_name = document.getElementById("name").value;
+    email = document.getElementById("email").value;
+    message = document.getElementById("message").value;
+    message = "Name: " + u_name + "\nEmail: " + email + "\nMessage: " + message;
+};
+var sender = function () {
+    ready();
+    var settings = {
+        "async": true,
+        "crossDomain": true,
+        "url": "https://api.telegram.org/bot" + telegram_bot_id + "/sendMessage",
+        "method": "POST",
+        "headers": {
+            "Content-Type": "application/json",
+            "cache-control": "no-cache"
+        },
+        "data": JSON.stringify({
+            "chat_id": chat_id,
+            "text": message
+        })
+    };
+    $.ajax(settings).done(function (response) {
+        console.log(response);
+    });
+    document.getElementById("name").value = "";
+    document.getElementById("email").value = "";
+    document.getElementById("message").value = "";
+    return false;
+};
+
+// ------------------------------------
+
+const soundCloud = document.querySelector('.sound-cloud');
+const off = document.querySelector('#off');
+const on = document.querySelector('#on');
+const myAudio = document.querySelector('#myAudio');
+
+off.addEventListener('click', () => soundTrack('off'));
+on.addEventListener('click', () => soundTrack('on'));
+
+const soundTrack = (soundState) => {
+    if(soundState === 'off'){
+        on.style.display = 'block';
+        off.style.display = 'none';
+        soundCloud.style.color = "#08fdd8";
+        myAudio.play();
+    }
+    else if(soundState === 'on'){
+        on.style.display = 'none';
+        off.style.display = 'block';
+        soundCloud.style.color = "#f50057";
+        myAudio.pause();
+    }
+}
+
+// Play music functionality
+
+const btnBars = document.querySelector('.bars');
+const btnTimes = document.querySelector('.times');
+const SideNav = document.querySelector('.aside');
+
+
+btnBars.addEventListener('click', () => myFunc('open'));
+btnTimes.addEventListener('click', () => myFunc('close'));
+
+const myFunc = (navCondition) => {
+    if(navCondition === 'open'){
+        SideNav.classList.add('show-nav');
+        btnTimes.style.display = "block";
+        btnBars.style.display = "none";
+    }
+    else if(navCondition === 'close'){
+            SideNav.classList.remove('show-nav');
+            btnTimes.style.display = "none";
+            btnBars.style.display = "block";
+    }
+}
+
